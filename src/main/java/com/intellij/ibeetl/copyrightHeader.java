62e8a6f0640e94783b0f7a1203f53753ecb8ff9e
trumans <trumans@sina.com>
2019-04-14 23:46:00 +0800

    添加语法描述文件，但未完善

--- src/main/java/com/intellij/ibeetl/copyrightHeader.java
+++ src/main/java/com/intellij/ibeetl/copyrightHeader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2016 Sergey Ignatov, Alexander Zolotov, Florin Patan
+ * Copyright 2019 trumansdo
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
