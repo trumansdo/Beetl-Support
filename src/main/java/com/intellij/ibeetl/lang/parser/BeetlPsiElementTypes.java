f1a483a95ebb96068241cc0528ff98f51ea7e4da
zengchao <1012243881@qq.com>
2019-06-06 18:10:17 +0800

    .

--- src/main/java/com/intellij/ibeetl/lang/parser/BeetlPsiElementTypes.java
+++ src/main/java/com/intellij/ibeetl/lang/parser/BeetlPsiElementTypes.java
@@ -4,13 +4,16 @@ import com.intellij.ibeetl.lang.base.BeetlIElementType;
 import com.intellij.ibeetl.lang.psi.elements.BeetlBinaryExpression;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
 
 public interface BeetlPsiElementTypes {
 	BeetlIElementType NUMBER = new BeetlIElementType("Number");
+	BeetlIElementType IDENTIFIER = new BeetlIElementType("Identifier");
 	BeetlIElementType BEETL_BLOCK = new BeetlIElementType("BeetlSyntaxBlock");
 	BeetlIElementType BINARY_EXPRESSION = new BeetlIElementType("BinaryExpression") {
 		public PsiElement createPsiElement(ASTNode node) {
 			return new BeetlBinaryExpression(node);
 		}
 	};
+	BeetlIElementType VAR_DEFINIED = new BeetlIElementType("VarDefinied");
 }
