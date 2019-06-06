4d639701d7cdccc8ecb9465a4c97cc13311ecda7
zengchao <1012243881@qq.com>
2019-06-06 18:10:17 +0800

    .

--- src/main/java/com/intellij/ibeetl/lang/parser/BeetlParser.java
+++ src/main/java/com/intellij/ibeetl/lang/parser/BeetlParser.java
@@ -32,6 +32,8 @@
 package com.intellij.ibeetl.lang.parser;
 
 import com.intellij.lang.pratt.*;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.Nullable;
 
 import static com.intellij.ibeetl.lang.lexer.BeetlIElementTypes.BTL_TEMPLATE_HTML_TEXT;
 import static com.intellij.ibeetl.lang.lexer.BeetlIElementTypes.VIRTUAL_ROOT;
@@ -41,7 +43,7 @@ import static com.intellij.ibeetl.lang.parser.BeetlPrattRegistry.registerParser;
 import static com.intellij.ibeetl.lang.parser.BeetlPsiElementTypes.BEETL_BLOCK;
 
 public class BeetlParser extends PrattParser {
-
+	public static final int INIT_LEVEL=0;
 
 	@Override
 	protected void parse(PrattBuilder builder) {
@@ -59,20 +61,26 @@ public class BeetlParser extends PrattParser {
 
 	static {
 		registerParser(BTL_TEMPLATE_HTML_TEXT, 1, AppendTokenParser.JUST_APPEND);
-		registerParser(BT_RDELIMITER, 1, AppendTokenParser.JUST_APPEND);
+//		registerParser(BT_RDELIMITER, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_LDELIMITER, 1, new TokenParser() {
 			@Override
 			public boolean parseToken(PrattBuilder prattBuilder) {
 				MutableMarker mark = prattBuilder.mark();
 				prattBuilder.advance();
-				prattBuilder.createChildBuilder(0).parse();
+				prattBuilder.createChildBuilder(INIT_LEVEL).parse();
 				prattBuilder.checkToken(BT_RDELIMITER);
 				mark.finish(BEETL_BLOCK);
 				return true;
 			}
 		});
 
-		registerParser(BT_IDENTIFIER, 1, AppendTokenParser.JUST_APPEND);
+		registerParser(BT_IDENTIFIER, 1, new AppendTokenParser() {
+			@Nullable
+			@Override
+			protected IElementType parseAppend(PrattBuilder prattBuilder) {
+				return BeetlPsiElementTypes.IDENTIFIER;
+			}
+		});
 		registerParser(BT_STRING, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_LPAREN, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_RPAREN, 1, AppendTokenParser.JUST_APPEND);
@@ -85,16 +93,16 @@ public class BeetlParser extends PrattParser {
 		registerParser(BT_PLACEHOLDER_VALUE, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_HTML_TAG_START, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_HTML_TAG_END, 1, AppendTokenParser.JUST_APPEND);
-		registerParser(BT_ASSIGN, 1, AppendTokenParser.JUST_APPEND);
+		registerParser(BT_ASSIGN, 1, TokenParser.infix(11,BeetlPsiElementTypes.VAR_DEFINIED));
 		registerParser(BT_ATTRIBUTE_NAME, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_ATTRIBUTE_VALUE, 1, AppendTokenParser.JUST_APPEND);
-		registerParser(BT_SEMICOLON, 12, AppendTokenParser.JUST_APPEND);
+		registerParser(BT_SEMICOLON, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_VAR, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_FOR, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_FOR_IN, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_IF, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_ELSE, 1, AppendTokenParser.JUST_APPEND);
-		registerParser(BT_INT, 11, AppendTokenParser.JUST_APPEND);
+		registerParser(BT_INT, 12, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_PLUS, 57, TokenParser.infix(10,BeetlPsiElementTypes.BINARY_EXPRESSION));
 		registerParser(BT_INCREASE, 1, AppendTokenParser.JUST_APPEND);
 		registerParser(BT_DOT, 1, AppendTokenParser.JUST_APPEND);
