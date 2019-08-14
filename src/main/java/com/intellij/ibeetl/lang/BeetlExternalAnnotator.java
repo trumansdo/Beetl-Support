7ec256218b866062ce0173062a88748af7b2522d
zengchao <1012243881@qq.com>
2019-08-14 18:04:31 +0800

    1、确定格式化无法完成 2、试着处理字符串引号 3、修正ajax段的语法识别

--- src/main/java/com/intellij/ibeetl/lang/BeetlExternalAnnotator.java
+++ src/main/java/com/intellij/ibeetl/lang/BeetlExternalAnnotator.java
@@ -8,17 +8,16 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiFile;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.beetl.core.Configuration;
 import org.beetl.core.GroupTemplate;
 import org.beetl.core.exception.BeetlException;
 import org.beetl.core.exception.ErrorInfo;
 import org.beetl.core.resource.StringTemplateResourceLoader;
+import org.beetl.core.statement.GrammarToken;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.intellij.ibeetl.setting.BeetlConfigure.*;
@@ -29,17 +28,19 @@ import static com.intellij.ibeetl.setting.BeetlConfigure.*;
  * collectInformation > doAnnotate > apply
  */
 public class BeetlExternalAnnotator extends ExternalAnnotator<BeetlExternalAnnotator.Info, BeetlExternalAnnotator.Result> {
-	
-	private static Logger logger = Logger.getInstance(BeetlExternalAnnotator.class);
-	
+
+	private static final Logger logger = Logger.getInstance(BeetlExternalAnnotator.class);
+
 	private Editor editor;
-	
+
 	private static GroupTemplate groupTemplate;
-	
+	/*NO_SUPPORT_ERROR 被定义为一个无法解析的错误信息*/
+	private static final String NO_SUPPORT_ERROR = "NO_SUPPORT_ERROR";
+
 	static {
 		resetConfig();
 	}
-	
+
 	public static void resetConfig() {
 		/*
 		 * 关于idea的classloader的几点：
@@ -47,7 +48,7 @@ public class BeetlExternalAnnotator extends ExternalAnnotator<BeetlExternalAnnot
 		 * 2. GroupTemplate.class.getClassLoader() 这种方式获取的classloader是idea为每一个plugin独立的classloader。也只有这种方式，才能加载到我们引入的第三方jar
 		 * 3. ClassLoader.getSystemClassLoader() 获取的classloader也是运行时idea环境的lib目录
 		 * */
-		
+
 		Thread currentThread = Thread.currentThread();
 		ClassLoader oldClassloader = currentThread.getContextClassLoader();
 		try {
@@ -66,7 +67,22 @@ public class BeetlExternalAnnotator extends ExternalAnnotator<BeetlExternalAnnot
 			currentThread.setContextClassLoader(oldClassloader);
 		}
 	}
-	
+
+	/**
+	 * 无论是否出现语法错误，都经过beetl本身走一次校验
+	 *
+	 * @param file      当前文件
+	 * @param editor    当前编辑器
+	 * @param hasErrors 是否出现语法错误
+	 * @return 自定义错误信息描述
+	 */
+	@Nullable
+	@Override
+	public Info collectInformation(@NotNull PsiFile file, @NotNull Editor editor, boolean hasErrors) {
+		this.editor = editor;
+		return this.collectInformation(file);
+	}
+
 	@Nullable
 	@Override
 	public Info collectInformation(@NotNull PsiFile file) {
@@ -75,38 +91,28 @@ public class BeetlExternalAnnotator extends ExternalAnnotator<BeetlExternalAnnot
 		 * 如果isValid() 是false，则表示当前文件不应该操作。
 		 * */
 		String content = ApplicationManager.getApplication()
-				                 .runReadAction((Computable<String>) () -> file.isValid() ? editor.getDocument().getText() : null);
+				.runReadAction((Computable<String>) () -> file.isValid() ? editor.getDocument().getText() : null);
 		if (StringUtils.isBlank(content)) return null;
-		String oldLine = System.getProperty("line.separator");
-		System.setProperty("line.separator", StringUtils.LF);
-		BeetlException beetlException = groupTemplate.validateTemplate(content);
-		System.setProperty("line.separator", oldLine);
-		if (null == beetlException) return null;
-		ErrorInfo errorInfo = beetlException.toErrorInfo();
+		ErrorInfo errorInfo;
+		BeetlException beetlException;
+		try {
+			beetlException = groupTemplate.validateTemplate(content);
+			if (null == beetlException) return null;
+			errorInfo = beetlException.toErrorInfo();
+		} catch (Exception e) {
+			beetlException = new BeetlException(NO_SUPPORT_ERROR, e);
+			beetlException.pushToken(new GrammarToken(NO_SUPPORT_ERROR, 0, 0));
+			errorInfo = new ErrorInfo(beetlException);
+		}
 		return new Info(file, errorInfo);
 	}
-	
-	/**
-	 * 无论是否出现语法错误，都经过beetl本身走一次校验
-	 *
-	 * @param file      当前文件
-	 * @param editor    当前编辑器
-	 * @param hasErrors 是否出现语法错误
-	 * @return
-	 */
-	@Nullable
-	@Override
-	public Info collectInformation(@NotNull PsiFile file, @NotNull Editor editor, boolean hasErrors) {
-		this.editor = editor;
-		return this.collectInformation(file);
-	}
-	
+
 	@Nullable
 	@Override
 	public Result doAnnotate(Info collectedInfo) {
 		String content = collectedInfo.getFile().getText();
 		String[] lines = content.split(StringUtils.LF);
-		List<Anno> annotations = new ArrayList<>();
+		Anno annotation;
 		ErrorInfo errorInfo = collectedInfo.getErrorInfo();
 		int errorTokenLine = errorInfo.getErrorTokenLine() - 1;
 		String errorTokenText = errorInfo.getErrorTokenText();
@@ -123,61 +129,63 @@ public class BeetlExternalAnnotator extends ExternalAnnotator<BeetlExternalAnnot
 			endOffset = startOffset + errorTokenText.length();
 			break;
 		}
-		TextRange textRange = new TextRange(startOffset, endOffset);
-		annotations.add(new Anno(textRange, errorInfo));
-		return new Result(annotations);
+		TextRange textRange;
+		if (StringUtils.equals(NO_SUPPORT_ERROR, errorTokenText)) {
+			textRange = new TextRange(0, 0);
+		} else {
+			textRange = new TextRange(startOffset, endOffset);
+		}
+		annotation = new Anno(textRange, errorInfo);
+		return new Result(annotation);
 	}
-	
+
 	@Override
 	public void apply(@NotNull PsiFile file, Result annotationResult, @NotNull AnnotationHolder holder) {
-		List<Anno> annotations = annotationResult.getAnnotations();
-		Anno anno = annotations.stream().findFirst().orElse(null);
-		holder.createErrorAnnotation(anno.getTextRange(), anno.getErrorInfo().getMsg());
+		Anno annotation = annotationResult.getAnnotation();
+		holder.createErrorAnnotation(annotation.getTextRange(), annotation.getErrorInfo().getMsg());
 	}
-	
+
 	@Override
 	public String getPairedBatchInspectionShortName() {
 		return "BeetlExternalLint";
 	}
-	
+
 	public static final class Info {
 		@NotNull
 		private final PsiFile file;
 		@NotNull
 		private final ErrorInfo errorInfo;
-		
+
 		public Info(@NotNull PsiFile file, ErrorInfo errorInfo) {
 			this.file = file;
 			this.errorInfo = errorInfo;
 		}
-		
+
 		@NotNull
 		public final PsiFile getFile() {
 			return this.file;
 		}
-		
+
 		@NotNull
 		public final ErrorInfo getErrorInfo() {
 			return this.errorInfo;
 		}
-		
+
 		@Override
 		public String toString() {
 			return "Info(file=" + this.file + ", errorInfo=" + errorInfo + ")";
 		}
-		
+
 		public int hashCode() {
 			return this.file != null ? this.file.hashCode() : 0;
 		}
-		
+
 		@Override
 		public boolean equals(@Nullable Object other) {
 			if (this != other) {
 				if (other instanceof Info) {
 					Info info = (Info) other;
-					if (this.file.equals(info.getFile()) && this.errorInfo.equals(info.getErrorInfo())) {
-						return true;
-					}
+					return this.file.equals(info.getFile()) && this.errorInfo.equals(info.getErrorInfo());
 				}
 				return false;
 			} else {
@@ -185,44 +193,42 @@ public class BeetlExternalAnnotator extends ExternalAnnotator<BeetlExternalAnnot
 			}
 		}
 	}
-	
+
 	public static final class Anno {
 		@NotNull
 		private final TextRange textRange;
 		@NotNull
 		private final ErrorInfo errorInfo;
-		
+
 		@NotNull
 		public TextRange getTextRange() {
 			return textRange;
 		}
-		
+
 		@NotNull
 		public final ErrorInfo getErrorInfo() {
 			return this.errorInfo;
 		}
-		
+
 		public Anno(TextRange textRange, @NotNull ErrorInfo errorInfo) {
 			this.textRange = textRange;
 			this.errorInfo = errorInfo;
 		}
-		
+
 		@NotNull
 		public String toString() {
 			return "Anno(textRange=" + this.textRange + ", errorInfo=" + this.errorInfo + ")";
 		}
-		
+
 		public int hashCode() {
 			return this.textRange.hashCode() + this.errorInfo.hashCode();
 		}
-		
+
 		public boolean equals(@Nullable Object other) {
 			if (this != other) {
 				if (other instanceof Anno) {
 					Anno anno = (Anno) other;
-					if (this.textRange.equals(anno.getTextRange()) && this.errorInfo.equals(anno.getErrorInfo())) {
-						return true;
-					}
+					return this.textRange.equals(anno.getTextRange()) && this.errorInfo.equals(anno.getErrorInfo());
 				}
 				return false;
 			} else {
@@ -230,36 +236,34 @@ public class BeetlExternalAnnotator extends ExternalAnnotator<BeetlExternalAnnot
 			}
 		}
 	}
-	
+
 	public static final class Result {
 		@NotNull
-		private final List<Anno> annotations;
-		
+		private final Anno annotation;
+
 		@NotNull
-		public final List<Anno> getAnnotations() {
-			return this.annotations;
+		public final Anno getAnnotation() {
+			return this.annotation;
 		}
-		
-		public Result(@NotNull List<Anno> annotations) {
-			this.annotations = annotations;
+
+		public Result(@NotNull Anno annotation) {
+			this.annotation = annotation;
 		}
-		
+
 		@NotNull
 		public String toString() {
-			return "Result(annotations=" + this.annotations + ")";
+			return "Result(annotations=" + this.annotation + ")";
 		}
-		
+
 		public int hashCode() {
-			return this.annotations != null ? this.annotations.hashCode() : 0;
+			return this.annotation != null ? this.annotation.hashCode() : 0;
 		}
-		
+
 		public boolean equals(@Nullable Object other) {
 			if (this != other) {
 				if (other instanceof Result) {
 					Result result = (Result) other;
-					if (CollectionUtils.isEqualCollection(this.annotations, result.getAnnotations())) {
-						return true;
-					}
+					return this.annotation.equals(result.getAnnotation());
 				}
 				return false;
 			} else {
--- src/main/java/com/intellij/ibeetl/lang/psi/formatter/BeetlBlock.java
+++ src/main/java/com/intellij/ibeetl/lang/psi/formatter/BeetlBlock.java
@@ -6,8 +6,10 @@ import com.intellij.formatting.templateLanguages.TemplateLanguageBlock;
 import com.intellij.formatting.templateLanguages.TemplateLanguageBlockFactory;
 import com.intellij.ibeetl.lang.lexer.BeetlIElementTypes;
 import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.tree.IElementType;
+import gherkin.lexer.Tr;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -22,56 +24,41 @@ public class BeetlBlock extends TemplateLanguageBlock {
 	protected BeetlBlock(@NotNull TemplateLanguageBlockFactory blockFactory, @NotNull CodeStyleSettings settings, @NotNull ASTNode node, @Nullable List<DataLanguageBlockWrapper> foreignChildren) {
 		super(blockFactory, settings, node, foreignChildren);
 		myNode = super.getNode();
+		System.out.println(myNode.getPsi()+" :=: "+myNode.getText()+" :=: "+myNode.getTextRange());
 	}
 	
 	@Nullable
 	@Override
 	public Spacing getSpacing(@Nullable Block child1, @NotNull Block child2) {
 		Spacing spacing = super.getSpacing(child1, child2);
-		return spacing;
+		return null;
 	}
-	
-	/**
-	 * Invoked when the current base language block is located inside a template data language block to determine the spacing after the current block.
-	 *
-	 * @param rightNeighbor  the block to the right of the current one
-	 * @param parent         the parent block
-	 * @param thisBlockIndex the index of the current block in the parent block sub-blocks
-	 * @return the spacing between the current block and its right neighbor
-	 */
+
 	@Nullable
 	@Override
 	public Spacing getRightNeighborSpacing(@NotNull Block rightNeighbor, @NotNull DataLanguageBlockWrapper parent, int thisBlockIndex) {
 		Spacing spacing = Spacing.createSpacing(0, 0, 0, false, 0, 0);
-		return spacing;
+		return null;
 	}
-	
-	/**
-	 * Invoked when the current base language block is located inside a template data language block to determine the spacing before the current block.
-	 *
-	 * @param leftNeighbor   the block to the left of the current one, or null if the current block is first child
-	 * @param parent         the parent block
-	 * @param thisBlockIndex the index of the current block in the parent block sub-blocks
-	 * @return the spacing between the current block and its left neighbor
-	 */
+
 	@Nullable
 	@Override
 	public Spacing getLeftNeighborSpacing(@Nullable Block leftNeighbor, @NotNull DataLanguageBlockWrapper parent, int thisBlockIndex) {
-		Spacing spacing = Spacing.createSpacing(0, 0, 0, false, 0, 0);
-		return spacing;
+		Spacing spacing = Spacing.createDependentLFSpacing(4, 4, new TextRange(0, 0), true, 0);
+		return null;
 	}
 	
 	@Nullable
 	@Override
 	public Wrap getWrap() {
-		Wrap wrap = Wrap.createWrap(WrapType.NONE, false);
+		Wrap wrap = Wrap.createWrap(WrapType.NONE, true);
 		return wrap;
 	}
 	
 	@Nullable
 	@Override
 	public Alignment getAlignment() {
-		Alignment alignment = Alignment.createAlignment(false, Alignment.Anchor.RIGHT);
+		Alignment alignment = Alignment.createAlignment(false, Alignment.Anchor.LEFT);
 		return alignment;
 	}
 	
@@ -80,7 +67,24 @@ public class BeetlBlock extends TemplateLanguageBlock {
 		Indent indent = Indent.getNoneIndent();
 		return indent;
 	}
-	
+
+	@Override
+	protected List<Block> buildChildren() {
+		return super.buildChildren();
+	}
+
+	@Override
+	protected Wrap createChildWrap(ASTNode child) {
+		Wrap wrap = Wrap.createWrap(WrapType.NONE, true);
+		return wrap;
+	}
+
+	@Override
+	protected Alignment createChildAlignment(ASTNode child) {
+		Alignment alignment = Alignment.createAlignment(true, Alignment.Anchor.RIGHT);
+		return alignment;
+	}
+
 	@Override
 	protected IElementType getTemplateTextElementType() {
 		return BeetlIElementTypes.BTL_TEMPLATE_HTML_TEXT;
